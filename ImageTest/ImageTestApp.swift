8b060b859cb50213a927528fa591d1e2b6853638
Sam Kim <damduc81@gmail.com>
2021-02-17 10:30:39 +0900

    Initial Commit

---
+++ ImageTest/ImageTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageTestApp.swift
+//  ImageTest
+//
+//  Created by damduc on 2021/02/17.
+//
+
+import SwiftUI
+
+@main
+struct ImageTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
